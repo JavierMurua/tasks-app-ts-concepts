e97b158dded8282d0549e48180ec9f67208f3211
Javier Murua <javierantoniomurua@gmail.com>
2025-02-21 20:03:03 -0300

    Remove redundancy in task filtering

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,3 @@
-// src/components/TaskList.tsx
 "use client";
 import { useTasks } from "@/context/TaskContext";
 import TaskItem from "./TaskItem";
@@ -6,30 +5,22 @@ import TaskFilter from "./TaskFilter";
 import ClearTaskButton from './ClearTasksButton'
 
 export default function TaskList() {
-  const { tasks, filter } = useTasks();
-
-  
-  const filteredTasks = tasks.filter((task) => {
-    if (filter === "all") return true;
-    if (filter === "completed") return task.completed;
-    if (filter === "pending") return !task.completed;
-    return true;
-  });
+  const { tasks } = useTasks();
 
   return (
     <div className="mt-8">
-            <div className="flex justify-between items-center mb-4">
-                <TaskFilter /> 
-                <ClearTaskButton />
+      <div className="flex justify-between items-center mb-4">
+        <TaskFilter />
+        <ClearTaskButton />
       </div>
 
-            {filteredTasks.length === 0 ? (
+      {tasks.length === 0 ? (
         <p className="text-center text-gray-200 mt-4">
           No tasks found! 📝
         </p>
       ) : (
         <div className="space-y-4">
-          {filteredTasks.map((task) => (
+          {tasks.map((task) => (
             <TaskItem key={task.id} task={task} />
           ))}
         </div>
